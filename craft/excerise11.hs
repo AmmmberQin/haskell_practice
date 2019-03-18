554d46cb0a845ac181a7d1828bc1d87b10ca598a
ariesduanmu <aries.duanmu@gmail.com>
2019-03-19 00:17:36 +0800

    11

--- craft/excerise11.hs
+++ craft/excerise11.hs
@@ -1,7 +1,7 @@
 -- @Author: ariesduanmu
 -- @Date:   2019-03-17 18:40:12
 -- @Last Modified by:   ariesduanmu
--- @Last Modified time: 2019-03-17 19:36:47
+-- @Last Modified time: 2019-03-18 23:11:17
 isChar :: Char -> Bool
 
 isChar c = not$elem c " \t\n" 
@@ -19,3 +19,26 @@ multiply x y = x*y
 
 doubleAll :: [Int] -> [Int]
 doubleAll = map (multiply 2)
+
+double :: Integer -> Integer
+double n = n * 2
+
+trible :: Integer -> Integer
+trible n = n * 3
+
+iter :: Integer -> (a -> a) -> a -> a
+iter n f
+  | n >=0 = f . iter (n-1) f
+  | otherwise = id
+
+iter1 :: Integer -> (a -> a) -> a -> a
+iter1 n f
+  | n >=0 = foldr1 (.) [f, iter1 (n-1) f]
+  | otherwise = id
+
+mapFuns :: [a->b] -> a -> [b]
+mapFuns fs x = map (\f->f x) fs
+
+mapFuns1 :: [a->b] -> a -> [b]
+mapFuns1 fs = funs1
+  where funs1 x = map (\f -> f x) fs
